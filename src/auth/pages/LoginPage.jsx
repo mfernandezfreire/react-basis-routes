f32ab9e286e12ed9c800b20f47cc13d7814ce205
mfernandezfreire <mfernandezfreir.ext@acciona.com>
2022-09-30 12:41:04 +0200

    Basic exercise without testing done by me

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,20 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+
+  const { login } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate('/', {
+
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
+    login('Manu Fernández Freire')
+
+    navigate(lastPath, {
       replace: true
     });
   }
@@ -15,9 +24,9 @@ export const LoginPage = () => {
       <h1>Login</h1>
       <hr />
 
-      <button 
+      <button
         className="btn btn-primary"
-        onClick={ onLogin }
+        onClick={onLogin}
       >
         Login
       </button>
